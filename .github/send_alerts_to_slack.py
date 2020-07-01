509f4742f01edde1f67b7b3fa1d97bf4ebfb8d76
Kevin McElwee <kevinrmcelwee@gmail.com>
2020-07-01 19:56:27 -0400

    add formatting for repeat use, use multiple attachments, use requests package instead of terminal command

--- .github/send_alerts_to_slack.py
+++ .github/send_alerts_to_slack.py
@@ -1,71 +1,95 @@
 """
-send_alerts_to_slack.py
 
 Summarize the Frictionless Data JSON error output and if there are any errors, 
-send a summary message to Slack. Also send a more detailed error summary to STDOUT
+send a summary message to a given Slack channel. Send a more detailed 
+error summary to std out and create a detailed txt file that can be accessed via GitHub
+artifacts.
+
+To run elsewhere, define SLACK_WEBHOOK in your repo's secrets and change the 
+top-level constants defined below. 
+
+`validate-csv.yml` can also be copied verbatim, assuming that the schema is 
+named `datapackage.json`.
+
+Links to...
+- Generate a Slack token
+    https://slack.com/apps/A0F7XDUAZ-incoming-webhooks
+- API for POST requests / formatting text:
+    https://api.slack.com/messaging
+
 
 """
 
 import json
+import requests
 import os
 
+ERROR_FILE = 'error_summary.json'
+REPO = 'Princeton-CDH/pemm-data'
+CHANNEL = '#slack-webhook-test'
+ERROR_MAX = 5
+
 def main():
-    with open('error_summary.json') as f: 
+    with open(ERROR_FILE) as f: 
         j = json.load(f)
 
-    # create std out message
-    giant_string = ""
-
-    giant_string += f"Error Count: {j['error-count']}\n"
+    # Create and print message to std out.
+    stdout_string = f"Error Count: { j['error-count'] }\n"
     for table in j['tables']:
         if table['error-count'] != 0:
-            giant_string += f"\tTable: {table['resource-name']}\n"
-            giant_string += f"\tError Count: {table['error-count']}\n"
-            for error in table['errors'][:5]:
-                giant_string += f'\t\t{error["message"]}\n'
-            giant_string += "\n"
+            stdout_string += f"\tTable: { table['resource-name'] }\n"
+            stdout_string += f"\tError Count: { table['error-count'] }\n"
+            for error in table['errors'][:ERROR_MAX]:
+                stdout_string += f'\t\t{ error["message"] }\n'
+            stdout_string += "\n"
 
-    print(giant_string)
+    print(stdout_string)
 
+    # Create a txt file that users can download as an artifact.
     with open('error_summary.txt', 'w') as f:
-        f.write(giant_string)
+        f.write(stdout_string)
 
-    # create slack message
-    if j['error-count'] != 0:
-        slack_message = ""
-        slack_message += f"Error Count: {j['error-count']}\n"
-        for table in j['tables']:
-            if table['error-count'] != 0:
-                slack_message += f"\tTable: {table['resource-name']}\n"
-                slack_message += f"\tError Count: {table['error-count']}\n"
-                slack_message += "\n"
+    # If there were no errors, end function and do not send a Slack message.
+    if j['error-count'] == 0:
+        return
 
-        slack_message = (
-            "It looks like there were a few errors in the latest data build." +
-            " Here's a summary:\n\n" + 
-                slack_message +
-            "For more details, head to <https://github.com/Princeton-CDH/" +
-            f"pemm-data/actions/runs/{os.environ['GITHUB_RUN_ID']}" + 
-            "?check_suite_focus=true|GitHub Actions>"
-        )
-        
-        j_payload = {
-            'channel': '#slack-webhook-test',
-            'username': 'webhookbot',
-            'text': slack_message
-        }
+    # Create a slack payload.
+    slack_message_prelude = (
+        f"🚨 Goodtables found {j['error-count']} error(s) in the latest sync." +
+        f" For more details, head to <https://github.com/{REPO}" +
+        f"/actions/runs/{os.environ['GITHUB_RUN_ID']}" + 
+        "?check_suite_focus=true|GitHub Actions>"
+    )
+    
+    # (This will be shown on mobile and notifications in lieu of attachments.)
+    slack_message_fallback = f"Error Count: { j['error-count'] }\n"
+    for table in j['tables']:
+        if table['error-count'] != 0:
+            slack_message_fallback += f"\tTable: { table['resource-name'] }\n"
+            slack_message_fallback += f"\tError Count: { table['error-count'] }\n"
+            slack_message_fallback += "\n"
 
-        s_payload = json.dumps(j_payload)
-        s_payload = s_payload.replace('"', '\\"')
+    j_payload = {
+        'channel': CHANNEL,
+        'text': slack_message_prelude,
+        'attachments': [
+            {
+                "fallback": slack_message_fallback,
+                "color": "danger",
+                "fields":[{
+                   "title": table['resource-name'] + '.csv',
+                   "value": (
+                        f"_Error Count_: { table['error-count'] }\n" +
+                        f"_Error type(s)_: {', '.join(set([x['code'] for x in table['errors']]))}\n"
+                    )
+                }]
+            } for table in j['tables'] if table['error-count'] != 0
+        ]
+    }
 
-        escaped_payload = 'payload="' + s_payload + '"'
+    # post stringified payload to url
+    requests.post(os.environ['SLACK_WEBHOOK'], json.dumps(j_payload))
 
-        command = 'curl -X POST --data-urlencode '
-        command += f'{escaped_payload} '
-        command += os.environ['SLACK_WEBHOOK']
-        print(command)
-
-        os.system(command)
 
 if __name__ == '__main__':
     main()
